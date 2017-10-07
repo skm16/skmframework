72281627a591ad8ae837a2cd00d327f3f1882414
Sean Roberts <sean@seankyleandmanley.com>
2017-10-07 18:11:47 -0400

    gives production vs development option for main js path

--- lib/register_scripts.php
+++ lib/register_scripts.php
@@ -4,7 +4,10 @@
  */
 function skmframework_scripts() {
     wp_enqueue_style( 'app-min-css', get_template_directory_uri() . '/assets/public/css/app.min.css' );
-    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/assets/js/vendor/jquery/jquery.min.js', array(), '3.1.1', true );;
-    wp_enqueue_script( 'main-theme-min-js', get_template_directory_uri() . '/assets/public/js/compiled.min.js', array(), '1.0.0', true );
+    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/assets/js/vendor/jquery/jquery.min.js', array(), '3.1.1', true );
+    /* -- UNCOMMENT WHEN READY FOR PRODUCTION
+    wp_enqueue_script( 'main-theme-min-js', get_template_directory_uri() . '/assets/public/js/compiled.min.js', array(), '1.0.0', true );*/
+    /* -- COMMENT OUT WHEN READY FOR PRODUCTION --*/
+    wp_enqueue_script( 'main-theme-js', get_template_directory_uri() . '/assets/js/compiled/compiled.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'skmframework_scripts' );
